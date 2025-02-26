54a5d77c483afc170ab48c8948cac698574927dd
Donizetet <dtsp75@google.com>
2025-02-26 02:53:19 -0300

    add  get, post e put

--- index.js
+++ index.js
@@ -4,7 +4,38 @@ const server = express();
 
 server.use(express.json());
 
-const cursos = ['FuççStack Master', 'Desenvolvimento de Games', 'Viver de Youtube'];
+const cursos = ['FullStack Master', 'Desenvolvimento de Games', 'Viver de Youtube'];
+
+// Retorna um curso
+server.get('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json(cursos[index]);
+});
+
+// Retornar todos os cursos
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
+
+// Criar um novo curso
+server.post('/cursos' , (req, res) => {
+    const { name } = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+});
+
+// Atualizar um curso
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
 
 
 
