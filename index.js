184f51c592a102ddca272f07abd6dd7c6d5c704f
Donizetet <dtsp75@google.com>
2025-02-26 02:27:06 -0300

    add. parte do código 01

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const cursos = ['FuççStack Master', 'Desenvolvimento de Games', 'Viver de Youtube'];
+
+
+
+
+
+
+
+
+
+
+
+server.listen(3000);
